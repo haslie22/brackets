daee93514e4baf0344c470119f4971ed0631c6d6
Lizaveta <lizadondysh@icloud.com>
2022-07-13 03:30:34 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let i = 0; i < str.length; i++) {
+    bracketsConfig.forEach(subarr => {
+      if (str[i] === subarr[0] && str[i + 1] === subarr[1]) {
+        str = str.slice(0, i) + str.slice(i + 2, str.length);
+        i -= 2;
+      }
+    });
+  }
+
+  return (str.length === 0);
 }
